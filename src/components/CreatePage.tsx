b0d6efebcd6d39de532c84da397af2502410102d
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-18 00:58:10 +0530

    show errors

--- src/components/CreatePage.tsx
+++ src/components/CreatePage.tsx
@@ -1,3 +1,5 @@
+import { Prisma } from "@prisma/client";
+import { Show } from "solid-js";
 import { createServerAction$, redirect } from "solid-start/server";
 import { prisma } from "~/server/db";
 
@@ -6,13 +8,24 @@ export const CreatePage = () => {
     // probably should validate this
     const name = form.get("name") as string;
 
-    await prisma.page.create({ data: { name } });
+    try {
+      await prisma.page.create({ data: { name } });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new Error("The provided name is already in use");
+        }
+      }
+    }
 
     return redirect(`/${name}`);
   });
 
   return (
     <Form class="flex flex-col gap-2">
+      <Show when={create.error}>
+        <span class="text-red-400">{create.error.message}</span>
+      </Show>
       <input
         type="text"
         name="name"
