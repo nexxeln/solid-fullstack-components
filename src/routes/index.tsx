f9484df8d071edbd907bfbc8c70ae508c01f2863
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-18 00:56:28 +0530

    fix width

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,14 @@ import { CreatePage } from "~/components/CreatePage";
 
 export default function Home() {
   return (
-    <main class="h-screen text-center font-sans bg-neutral-900 text-neutral-200 flex flex-col items-center">
+    <main class="h-screen text-center font-sans bg-neutral-900 text-neutral-200 flex flex-col items-center w-screen">
       <h1 class="text-4xl pt-6 font-black">
         Full Stack <span class="text-#86BDE6">Solid</span> Components
       </h1>
 
-      <div class="pt-10" />
-      <CreatePage />
+      <div class="pt-10 w-1/3">
+        <CreatePage />
+      </div>
     </main>
   );
 }
