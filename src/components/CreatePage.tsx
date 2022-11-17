18f639a074623124947318bfb1251438019a55d5
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-17 16:06:56 +0530

    feat: form done

---
+++ src/components/CreatePage.tsx
@@ -0,0 +1,31 @@
+import { createServerAction$, redirect } from "solid-start/server";
+import { prisma } from "~/server/db";
+
+export const CreatePage = () => {
+  const [create, { Form }] = createServerAction$(async (form: FormData) => {
+    // probably should validate this
+    const name = form.get("name") as string;
+
+    await prisma.page.create({ data: { name } });
+
+    return redirect(`/${name}`);
+  });
+
+  return (
+    <Form class="flex flex-col gap-2">
+      <input
+        type="text"
+        name="name"
+        id="name"
+        class="bg-neutral-800 px-4 py-2 border border-zinc-700 rounded focus:outline-none focus:ring focus:ring-zinc-600"
+      />
+      <button
+        type="submit"
+        disabled={create.pending}
+        class="font-semibold text-xl disabled:text-neutral-400"
+      >
+        {create.pending ? "Creating..." : "Create"}
+      </button>
+    </Form>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
+import { CreatePage } from "~/components/CreatePage";
+
 export default function Home() {
   return (
-    <main class="h-screen text-center font-sans bg-neutral-900 text-neutral-200">
+    <main class="h-screen text-center font-sans bg-neutral-900 text-neutral-200 flex flex-col items-center">
       <h1 class="text-4xl pt-6 font-black">
         Full Stack <span class="text-#86BDE6">Solid</span> Components
       </h1>
+
+      <div class="pt-10" />
+      <CreatePage />
     </main>
   );
 }
